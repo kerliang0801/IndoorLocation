3e584c7442f7f29161539729805ad67bdb785a8e
Christopher Lau <christopherlaukl@gmail.com>
2019-04-23 23:54:00 -0700

    first commit

---
+++ IndoorLocation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IndoorLocation
+//
+//  Created by KL on 23/04/2019.
+//  Copyright © 2019 KL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
